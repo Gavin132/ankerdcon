6902260f4489956d5b7ae5759a72b39960bd43cb
youb01 <lfatmi.ayoub@gmail.com>
2026-06-20 23:14:51 +0200

    Updated classes

--- backend/app/models/payment.py
+++ backend/app/models/payment.py
@@ -1,12 +1,18 @@
 from pydantic import BaseModel
 
 
+class Split(BaseModel):
+    name: str
+    amount: float
+
+
 class Payment(BaseModel):
     row_number: int
     paid_by: str
     amount: float
     description: str
     date: str
+    splits: list[Split] = []
 
 
 class CreatePaymentRequest(BaseModel):
@@ -14,3 +20,4 @@ class CreatePaymentRequest(BaseModel):
     amount: float
     description: str
     date: str
+    splits: list[Split] = []
