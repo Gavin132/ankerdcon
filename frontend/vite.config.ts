822fb8c950aed528c7bbf8598cd23eccdbfec15a
gavin132_ <75799630+Gavin132@users.noreply.github.com>
2026-06-21 00:17:15 +0000

    updated server block

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['con.ankerd.org'], // <--- THIS IS THE NEW LINE
     proxy: {
       "/api": {
         target: "http://localhost:8000",
